c5896c7d8b49ae95edd4978c0457c2549a051399
rajiv256 <rajivpensidpri@gmail.com>
2021-11-04 22:04:31 -0400

    Updated 4.1, 4.2

--- online_greedy_4_1.py
+++ online_greedy_4_1.py
@@ -2,44 +2,71 @@ import os
 import numpy as np
 import random
 
-def online_greedy_step(B, M, W, n, kw_num):
 
+# In the online setting, "m" value is not known. We do know "r" which is the no. of unique key words.
+
+def online_greedy_step(B, M, W, n, kw_num):
+    """
+
+    Args:
+        B:
+        M:
+        W:
+        n:
+        kw_num: keyword number.
+
+    Returns:
+        optimal_ad_num: which advertiser is mapped.
+        optimal_bid: bid value of the matched advertiser
+    """
     optimal_ad_num = -1
     optimal_bid = 0
     for i in range(n):
-        if W[i][kw_num] <= B[i]-M[i]:
-            if optimal_bid < W[i][kw_num]:
+        if W[i][kw_num] <= (B[i]-M[i]):
+            if optimal_bid <= W[i][kw_num]:
                 optimal_bid = W[i][kw_num]
                 optimal_ad_num = i
     return optimal_ad_num, optimal_bid
 
 
-def online_greedy(B, W, n, m):
+def online_greedy(B, W, n, r, kw_nums):
+    """
+    Args:
+        B: (n), budgets
+        W: (n*r) i-th advertiser's bid for j-th keyword
+        n: # advertisers
+        r: # keywords
+        kw_nums: (m) number of the keyword in each of the queries
 
-    M = [0]*n
+    Returns:
+        Q: (m) query assignment
+        revenue: Revenue accrued
+    """
+    M = [0] * n
+    m = len(kw_nums)
     revenue = 0
     Q = [-1]*m
 
-    for i in range(m):
-
-        ad_num, bid = online_greedy_step(B, M, W, n, i)
+    for t in range(len(kw_nums)):
+        kw_num = kw_nums[t]
+        ad_num, bid = online_greedy_step(B, M, W, n, kw_num)
         if ad_num == -1:
             continue
-            
         M[ad_num] += bid
         revenue += bid
-        Q[i] = ad_num
+        Q[t] = ad_num
     return Q, revenue
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # When testing, substitute the variables n, m, W, B with appropriate values.
     n = 4
-    m = 2
+    r = 2
+    m = 10
     W = np.random.randint(1, 10, (n, m))
     B = np.random.randint(2, 10, (n))
-
-    Q, revenue = online_greedy(B, W, n, m)
+    kw_nums = np.random.randint(0, r, (m))
+    Q, revenue = online_greedy(B, W, n, r, kw_nums)
     print(B)
     print(W)
     print(Q)
--- online_weighted_greedy_4_2.py
+++ online_weighted_greedy_4_2.py
@@ -7,40 +7,53 @@ def discount(b, m):
 
 
 def online_weighted_greedy_step(B, M, W, n, kw_num):
-
     optimal_ad_num = -1
     optimal_bid = 0
     for i in range(n):
-        if W[i][kw_num] <= B[i]-M[i]:
+        if W[i][kw_num] <= (B[i] - M[i]):
             if optimal_bid < discount(B[i], M[i])*W[i][kw_num]:
                 optimal_bid = W[i][kw_num]
                 optimal_ad_num = i
     return optimal_ad_num, optimal_bid
 
 
-def online_weighted_greedy(B, W, n, m):
+def online_weighted_greedy(B, W, n, r, kw_nums):
+    """
 
+    Args:
+        B:
+        W:
+        n:
+        r:
+        kw_nums:
+
+    Returns:
+
+    """
     M = [0]*n
     revenue = 0
+    m = len(kw_nums)
     Q = [-1]*m
 
-    for i in range(m):
-
-        ad_num, bid = online_weighted_greedy_step(B, M, W, n, i)
-
-        # No possible allocation exists.
+    for t in range(m):
+        kw_num = kw_nums[t]
+        ad_num, bid = online_weighted_greedy_step(B, M, W, n, kw_num)
         if ad_num == -1:
             continue
-        
         M[ad_num] += bid
         revenue += bid
-        Q[i] = ad_num
+        Q[t] = ad_num
     return Q, revenue
 
 
 if __name__=="__main__":
     # When testing, substitute the variables n, m, W, B with appropriate values.
     n = 4
-    m = 2
+    r = 2
+    m = 10
     W = np.random.randint(1, 10, (n, m))
     B = np.random.randint(2, 10, (n))
+    kw_nums = np.random.randint(0, r, (m))
+    Q, revenue = online_weighted_greedy(B, W, n, r, kw_nums)
+    print(Q)
+    print(revenue)
